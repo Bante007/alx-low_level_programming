fcf47028d518045160a4f2e6dcfdef41bc705035
bante007 <david505.cd@gmail>
2023-10-05 15:55:41 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,15 +7,17 @@
  */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 0;
+	const char *message = 	"and that piece of art is uesful\" - "
+				"Dora Korpar, 2015-10-19\n":
+	ssize_t len = 0;
 
-    while (message[len] != '\0')
-    {
-        len++;
-    }
+	while (message[len] != '\0')
+	{
+		len++;
+	}
 
-    write(2, message, len); /* Use write to print to standard error (file descriptor 2) */
+	write(2, message, len); /* Use write to print to standard error
+				   (file descriptor 2) */
 
-    return (1); /* Return 1 to indicate an error */
+	return (1); /* Retunr 1 to indicate an error */
 }
