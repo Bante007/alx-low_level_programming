b638e41f8c6883e5a1ded138a42a00dfcb7863f1
bante007 <david505.cd@gmail>
2023-10-05 08:39:57 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,10 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle,); /* Print "Programming is like building a multilingual puzzle to the screen */
-    return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
 }
+
