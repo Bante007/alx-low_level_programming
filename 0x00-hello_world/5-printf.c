adcdbb7a2bcc3fecb17c135f4d9fb0f447bff0bf
bante007 <david505.cd@gmail>
2023-10-05 09:26:22 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /**
  * main - Entry point of the program
  *
+ * Description: This function is the entry point of the program. It prints
+ *              a message to the standard output with double quotation marks.
+ *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
