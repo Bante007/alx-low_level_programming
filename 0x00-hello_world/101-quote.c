e56f63d9ef6b7dc47fa2a7a377947f19cf2f1d42
bante007 <david505.cd@gmail>
2023-10-05 15:25:58 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (error)
+ */
+int main(void)
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 0;
+
+    while (message[len] != '\0')
+    {
+        len++;
+    }
+
+    write(2, message, len); /* Use write to print to standard error (file descriptor 2) */
+
+    return (1); /* Return 1 to indicate an error */
+}
