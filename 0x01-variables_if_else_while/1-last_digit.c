474d159e872aa54da9bb5da71c0b4765c9961784
bante007 <david505.cd@gmail>
2023-10-06 15:30:54 -0700

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the last digit of any random number
+ * using the printf function. Each program is printed on a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
