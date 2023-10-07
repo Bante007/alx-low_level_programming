3f9ec2853f13be67e89148ba5318ac566ddc50fd
bante007 <david505.cd@gmail>
2023-10-07 09:23:01 -0700

    Variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,17 +5,16 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-        char letter = 'a';
+	char letter = 'a';
 
-        while (letter <= 'z')
-        {
-                putchar(letter);
-                letter++;
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
         }
+	putchar('\n');
 
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
