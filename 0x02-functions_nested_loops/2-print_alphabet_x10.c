fb476a0d0dae8929462f0eb5b293ff24f84e2d61
bante007 <david505.cd@gmail>
2023-10-10 18:59:00 -0700

    Alphabet X10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,13 +13,13 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		c = 'a';
+		while (c <= 'z')
 		{
 
 
-			_putchar(ch);
-			ch++;
+			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
 		i++;
