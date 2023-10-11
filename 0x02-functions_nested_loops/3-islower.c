4af9db771c26dce2573d998be86f359550a2c19d
bante007 <david505.cd@gmail>
2023-10-10 19:06:50 -0700

    Islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks lowercase character
- * c is the character tto be checked
+ * @c is the character to be checked
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
