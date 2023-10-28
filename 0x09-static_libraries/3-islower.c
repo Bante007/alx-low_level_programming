22116a30555605a59e1d7e37de897fd94f194406
bante007 <david505.cd@gmail>
2023-10-28 09:36:02 -0700

    Creation of Static Library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercase character
+ * @c is the character to be checked
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+
+	return (c >= 'a' && c <= 'z');
+}
